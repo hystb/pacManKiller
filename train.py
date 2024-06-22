a4d7e2b956c0aea93e8ca9853f1c4586d172707d
milan <milan.nicolas@rocketmail.com>
2024-06-22 18:53:57 +0200

    change train reward

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import torch
 import torch.optim as optim
 import torch.nn as nn
 import random
+import numpy as np
 
 # import cv2
 # import numpy as np
@@ -13,16 +14,15 @@ import random
 
 def train(model, env, num_episodes, batch_size, gamma, epsilon, epsilon_decay, min_epsilon, learning_rate, device):
     model.to(device)
-    static_count = 1
     max_reward = 0
     count_frame = 1
+    normalization_factor = 100000
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     loss_fn = nn.MSELoss()
     replay_buffer = deque(maxlen=10000)
     all_rewards = []
 
     for episode in range(num_episodes):
-        last_info = {'ale.lives': 3}
         state = env.reset()
         stacked_frames = deque([preprocess(state) for _ in range(4)], maxlen=4)
         stacked_state, _ = stack_frames(stacked_frames, state, True, preprocess)
@@ -33,45 +33,34 @@ def train(model, env, num_episodes, batch_size, gamma, epsilon, epsilon_decay, m
             if random.random() < epsilon:
                 action = env.action_space.sample()
             else:
+                model.eval()
                 with torch.no_grad():
                     q_values = model(stacked_state)
                     action = torch.argmax(q_values).item()
+                model.train()
 
             next_state, reward, done, info = env.step(action)
-            if info['ale.lives'] < last_info['ale.lives']:
-                last_info = info
-                reward -= 50
-            if reward == 0:
-                static_count += 1
-            else:
-                static_count = 1
-            if static_count % 100 == 0:
-                reward -= 20
-            # frame = env.render(mode='rgb_array')
+            
+            reward = reward / normalization_factor
+            
             total_reward += reward
             
             next_stacked_state, stacked_frames = stack_frames(stacked_frames, next_state, False, preprocess)
             
             replay_buffer.append((stacked_state, action, reward, next_stacked_state, done))
             stacked_state = next_stacked_state.to(device)
-            
-            # resized_frame = cv2.resize(frame, window_size)
-            # cv2.imshow('Ms. Pacman', resized_frame)
-            
-            # if cv2.waitKey(1) & 0xFF == ord('q'):
-            #     break
 
             if len(replay_buffer) >= batch_size:
                 batch = random.sample(replay_buffer, batch_size)
                 states, actions, rewards, next_states, dones = zip(*batch)
 
-                states = torch.cat(states).to(device)
+                states = torch.stack(states).to(device).squeeze(1)
                 actions = torch.tensor(actions).to(device)
                 rewards = torch.tensor(rewards, dtype=torch.float32).to(device)
-                next_states = torch.cat(next_states).to(device)
+                next_states = torch.stack(next_states).to(device).squeeze(1)
                 dones = torch.tensor(dones, dtype=torch.float32).to(device)
 
-                current_q_values = model(states).gather(1, actions.view(-1, 1)).squeeze()
+                current_q_values = model(states).gather(1, actions.unsqueeze(1)).squeeze(1)
                 next_q_values = model(next_states).max(1)[0]
                 expected_q_values = rewards + (gamma * next_q_values * (1 - dones))
 
@@ -91,9 +80,9 @@ def train(model, env, num_episodes, batch_size, gamma, epsilon, epsilon_decay, m
         if total_reward > max_reward:
             max_reward = total_reward
             timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-            model_save_path = f"modelmax{total_reward}_{timestamp}.pth"
+            model_save_path = f"modelmax{total_reward*normalization_factor}_{timestamp}.pth"
             torch.save(model.state_dict(), model_save_path)
-        if episode % 10 == 0:
+        if episode % 1000 == 0:
             timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
             model_save_path = f"model_{timestamp}.pth"
             torch.save(model.state_dict(), model_save_path)
